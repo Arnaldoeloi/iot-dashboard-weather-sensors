fd0a2ce77774f8e5c117d24831f5d625734a2c92
Arnaldo Souza <arnaldoeloi2@gmail.com>
2021-07-15 01:11:37 -0300

    Requirements updated.

--- core/settings.py
+++ core/settings.py
@@ -123,3 +123,8 @@ STATICFILES_DIRS = (
 )
 #############################################################
 #############################################################
+
+# ie if Heroku server
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
