fdb79689064bdfd860c75e0736e86e05f413dece
Arnaldo Souza <arnaldoeloi2@gmail.com>
2021-07-15 00:23:21 -0300

    Init

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# Change Log
+
+## [1.0.1] 2021-02-10
+### Improvements
+
+- Bump UI: [Jinja Material](https://github.com/app-generator/jinja-material-dashboard) v1.0.2
+- Bump Codebase: [Django Dashboard](https://github.com/app-generator/boilerplate-code-django-dashboard) v1.0.4
+
+### 2017-06-20 - Bug Fix
+Left menu selection based on the current page. Modified files:
+- app\views.py
+- core\templates\includes\sidebar-rtl.html
+
+## [1.0.0] 2020-07-30
+### Initial Release
---
+++ core/urls.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.contrib import admin
+from django.urls import path, include  # add this
+from app.views import graphTemp 
+
+urlpatterns = [
+    path('admin/', admin.site.urls),          # Django admin route
+    path("api/sensors/temp", graphTemp),
+    path("", include("authentication.urls")), # Auth routes - login / register
+    path("", include("app.urls"))             # UI Kits Html files
+]
