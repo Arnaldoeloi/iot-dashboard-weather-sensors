cfca9b24bebd70e43064ef7c78cfc7b6ef3af128
Arnaldo Souza <arnaldoeloi2@gmail.com>
2021-08-05 17:12:33 -0300

    Added iot01.arnaldosouza.com to allowed hosts.

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1'), 'kharkiv-ukraine-sensors.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1'), 'kharkiv-ukraine-sensors.herokuapp.com', 'iot01.arnaldosouza.com']
 
 # Application definition
 
